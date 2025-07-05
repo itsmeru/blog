f13c05c7c2bdfab0568a98b3041339f44b26d32f
itsmeru <cat821016@gmail.com>
2025-07-05 15:49:40 +0800

    feat: register and login

--- blogsite/accounts/models.py
+++ blogsite/accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.utils import timezone
 
 class User(AbstractUser):
     birth_date = models.DateField(blank=True, null=True, verbose_name="生日")
+    REQUIRED_FIELDS = ['email', 'first_name']
+    def __str__(self):
+        return self.username
--- blogsite/accounts/views.py
+++ blogsite/accounts/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.contrib import messages
+
+from .models import User
+
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        account = request.POST['account']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        birth_date = request.POST['birth_date']
+
+        if not all([account, username, email, password, birth_date]):
+            messages.error(request, '請填寫完整資料')
+            return render(request, 'accounts/register.html')
+        
+        if User.objects.filter(username=username).exists():
+            messages.error(request, '帳號已存在')
+            return render(request, 'accounts/register.html')
+        
+    return render(request, 'accounts/register.html')
+
+def login(request):
+    return render(request, 'accounts/login.html')
